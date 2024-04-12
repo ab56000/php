9a357ef4250be85e74bb0b3804ecd78448b69d05
ab56000 <134676103+ab56000@users.noreply.github.com>
2024-04-12 11:36:41 +0800

    Update and rename php1 to 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,10 @@
+<html>
+    <head><title>html+php</title></head>  <!--echo用php之輸出-->
+    <body>
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+    </body>
+</html>
